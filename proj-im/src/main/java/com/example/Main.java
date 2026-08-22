f04442f8bd17042690b764ca8c06b7b3a4df3d3d
GTnoBRAND <jaskaamanbayev2003@gmail.com>
2026-08-22 12:16:37 +0500

    Example of using arithmetic operator with simple currency conversion

--- proj-im/src/main/java/com/example/Main.java
+++ proj-im/src/main/java/com/example/Main.java
@@ -2,6 +2,31 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+
+        //using arithmetic operations
+
+        double amountInUSD = 200;
+
+        double exchangeRateUSDtoEUR = 0.85;
+
+        System.out.println("Amount in USD: " + amountInUSD);
+
+        double amountInEUR = amountInUSD *exchangeRateUSDtoEUR;
+
+        System.out.println("Amount in EUR: " + amountInEUR);
+
+        int expenditureForBooksINEUR = 55;
+
+        double remaining = amountInEUR - expenditureForBooksINEUR;
+
+        System.out.println("Remaining amount after expenditure: " + remaining);
+
+        double EURtoUSD = 1.18;
+
+        double inUSD = remaining * EURtoUSD;
+
+        System.out.println("Remaining amount in USD: " + inUSD);
+
     }
 }
